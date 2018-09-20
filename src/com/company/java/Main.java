c53bc3141294bdc184bbbb9860bc2404c3a10440
yashkudalkar <yashkudalkar1217@gmail.com>
2018-09-20 06:01:56 +0530

    mend

--- src/com/company/java/Main.java
+++ src/com/company/java/Main.java
@@ -3,7 +3,8 @@ package com.company.java;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// parent child constrictor example :- parent contructor is called every when child object is created but parent object
+        //   is not created
         System.out.println("Hello world!!!");
         Parent parent=new Parent();
         Child child=new Child();
